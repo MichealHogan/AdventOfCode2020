18b81b20c0c618b6e0cf0203c651fb5725df18b5
Micheal_Hogan <michealhogan1997@gmail.com>
2020-12-02 22:52:26 +0000

    :christmas_tree: Puzzles1&2+input

---
+++ Day1/puzzle1part1.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class puzzle1part1 {
+    public static void main(String[] args){
+        List<Integer> inputs = loadWordsFromFile("input.txt");
+        for(int i = 0; i < inputs.size(); i++){
+            int first = inputs.get(i);
+            for(int j = i + 1; j < inputs.size(); j++){
+                int second = inputs.get(j);
+                if(first + second == 2020){
+                    System.out.println("Answer to the puzzle is " + first * second);
+                }
+            }
+        }
+    }
+    private static List<Integer> loadWordsFromFile(String filename) {
+        try {
+            FileReader aFileReader = new FileReader(filename);
+            BufferedReader aBufferReader = new BufferedReader(aFileReader);
+            List<Integer> numbers = new ArrayList<Integer>();
+            String aNumber;
+            aNumber = aBufferReader.readLine() ;
+
+            while (aNumber != null) {
+                int result = Integer.parseInt(aNumber);
+                numbers.add(result);
+                aNumber = aBufferReader.readLine() ;
+            }
+
+            aBufferReader.close();
+            aFileReader.close();
+            return numbers ;
+        }
+        catch(IOException x) {
+            return null ;
+        }
+    }
+}
